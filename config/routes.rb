a222cefaf43361a2c7722cab0f6f7814849764a6
gurucharan <gurucharan@saastrail.com>
2024-02-18 18:27:55 +0530

    delete

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
     namespace :v1 do 
       get 'getUser' => 'books#index'
       post 'createUser' => 'books#create'
-      delete 'deleteUser/:id', to: 'books#delete
+      delete 'deleteUser/:id', to: 'books#delete'
     end
   end
 
